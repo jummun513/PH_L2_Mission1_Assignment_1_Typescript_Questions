a05e06ac2f75552633f4321c6d51cefa0d354496
jummun513 <jummunislam513@gmail.com>
2023-11-09 19:33:28 +0600

    add solved problem

---
+++ task.ts
@@ -0,0 +1,111 @@
+// problem --> 1
+const lengthOrSquare = (param: number | string): number | undefined => {
+    if (typeof param === 'number') {
+        return param * param;
+    }
+    else if (typeof param === 'string') {
+        return param.length;
+    }
+}
+
+
+// problem --> 2
+interface Person {
+    address?: {
+        city: string;
+        street: string;
+    }
+    phone?: number;
+}
+const getAddressCity = (param: Person): string | undefined => {
+    return param.address?.city;
+}
+
+
+// problem --> 3
+class Cat {
+    name: string;
+    origin: string;
+    age: number;
+    constructor(name: string, origin: string, age: number) {
+        this.name = name;
+        this.origin = origin;
+        this.age = age;
+    }
+}
+const isCat = (param: Cat) => {
+    if (param instanceof Cat) {
+        return console.log("yes, it's a cat.");
+    }
+    else {
+        return console.log("no, it's not a cat.");
+    }
+}
+
+
+//problem --> 4
+const addNumber = (mixedData: (number | string)[]): number => {
+    let sum = 0;
+    for (const item of mixedData) {
+        if (typeof item === 'number') {
+            sum = sum + item;
+        }
+        else {
+            sum = sum;
+        }
+    }
+    return sum;
+}
+
+
+//problem --> 5
+interface Car {
+    make: string;
+    model: string;
+    year: number;
+}
+interface Driver {
+    name: string;
+    licenseNumber: number;
+}
+const combinedTwoObject = (car: Car, driver: Driver) => {
+    const newCombinedObject = {
+        ...car, ...driver
+    }
+    return newCombinedObject;
+}
+
+
+// problem --> 6
+const checkArrayOfNumber = (array: unknown) => {
+    if (Array.isArray(array)) {
+        let sum = 0;
+        for (const item of array) {
+            if (typeof item === 'number') {
+                sum = sum + item;
+            }
+            else {
+                return console.log('Provided array contains NaN.');
+            }
+        }
+        return console.log(`Sum is = ${sum}`);
+    }
+    else {
+        return console.log('Not an array.')
+    }
+}
+
+
+// problem --> 8
+interface Product {
+    name: string;
+    price: number;
+    quantity: number;
+}
+const totalCost = (shoppingCart: Product[]): number => {
+    let sum = 0;
+    for (const item of shoppingCart) {
+        sum = sum + item.price * item.quantity;
+    }
+    return sum;
+}
